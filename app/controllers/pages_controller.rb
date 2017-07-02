1f99377377793837838a619f210b0b9b9c1f461f
Syrbain <syrbaincellier@gmail.com>
2017-07-02 16:58:24 +0200

    premier commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
+  end
+
+  def team
+    @members = ["Bob", "Romain", "Seb"]
+  end
+
+  def join_us
+    render plain: "Send us a mail"
   end
 end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,7 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = ["Kudoz", "uSlide", "Hubum", "Roadster"]
+  end
+
+end
