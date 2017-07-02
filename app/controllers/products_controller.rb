1a67568c916621e74aa2540e9480353fe7c70268
Syrbain <syrbaincellier@gmail.com>
2017-07-03 00:06:13 +0200

    DB & validator

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,11 @@
 class ProductsController < ApplicationController
 
-  PRODUCTS = [
-            {name: "Kudoz", category: "pro"},
-            {name: "uSlide", category: "education"},
-            {name: "Hubum", category: "lifestyle"},
-            {name: "Roadstr", category: "lifestyle"}
-          ]
   def index
-    if params[:filter]
-      @products = PRODUCTS.select {|product| product[:category] == params[:filter]}
-    else
-      @products = PRODUCTS
-    end
+    @products = Product.all
   end
 
   def show
-    @product = PRODUCTS[params[:id].to_i]
+    @product = Product.find(params[:id])
   end
 
 end
