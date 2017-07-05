e3f22e3b0fc833dc93a9087ea98c34308d61a44f
Syrbain <syrbaincellier@gmail.com>
2017-07-05 12:37:04 +0200

    implement action new & create on products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,16 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,6 +28,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  #strong params
+  def product_params
+    params.require(:product).permit(:name, :url)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+Product.create!(name: "Kudoz", url: "wwww.kudoz.com", tagline: "Tinder for job search")
+Product.create!(name: "uSlide", url: "wwww.uslide.io", tagline: "YouTube sucks for education")
+Product.create!(name: "Medpics", url: "wwww.medpics.com", tagline: "Share your diagnostics")
