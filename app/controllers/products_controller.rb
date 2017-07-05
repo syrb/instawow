3b15dfa34fa4f03509f2c99c3f3cb3301c405fef
Syrbain <syrbaincellier@gmail.com>
2017-07-05 13:18:17 +0200

    set up update & delete + refacto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,15 @@
 class ProductsController < ApplicationController
 
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
-
   def show
-    @product = Product.find(params[:id])
   end
-
   def new
     @product = Product.new
   end
-
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -20,19 +18,27 @@ class ProductsController < ApplicationController
       render :new
     end
   end
-
   def edit
   end
-
   def update
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
-
   def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   #strong params
+  private
   def product_params
     params.require(:product).permit(:name, :url)
+  end
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
