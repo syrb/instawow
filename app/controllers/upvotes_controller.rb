cfc796c4b89a7dff89cd379ccb9de6b082f18ebf
Syrbain <syrbaincellier@gmail.com>
2017-07-18 18:47:23 +0200

    ajax upvote

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,15 +1,25 @@
 class UpvotesController < ApplicationController
 
   def create
-    product = Product.find(params[:product_id])
-    product.upvotes.create! user: current_user
-    redirect_to products_path
+    @product = Product.find(params[:product_id])
+    @product.upvotes.create! user: current_user
+
+    respond_to do |format|
+      format.html {redirect_to products_path}
+      format.js
+    end
   end
 
   def destroy
     upvotes = Upvote.find(params[:id])
+    @product = upvotes.product
+
     upvotes.destroy
-    redirect_to products_path
+
+    respond_to do |format|
+      format.html {redirect_to products_path}
+      format.js
+    end
   end
 
 end
